a8fc2a045686c897bc27daa20bf78c7728ac5a0b
Nikhil Kumar Ghanghor <nkghanghor18bcs@iiitkottayam.ac.in>
2022-03-22 10:34:49 +0530

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -111,6 +111,7 @@ def get_dataloader_triplets(root_dir,val_size,
             # transforms.RandomRotation(15),
             # transforms.RandomResizedCrop(224),
             # transforms.RandomHorizontalFlip(),
+            transforms.Resize(128),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])])
